8d70d5095671d36db0c1417cd7364ac3a07d8ae5
VivekGujale <vdgujale007@.com>
2022-10-06 19:17:43 +0530

    added logic to calculate Employee wage assuming partTime 8

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,16 +1,19 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int isFullTime = 1;
+        int isPartTime = 1;
+        int isFullTime = 2;
         int empRatePerHr = 20;
         int empHrs, empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10 % 2);
-        if (empCheck == isFullTime) {
+        double empCheck = Math.floor(Math.random() * 10 % 3);
+        if (empCheck == isPartTime) {
             empHrs = 8;
+        } else if (empCheck == isFullTime) {
+            empHrs = 16;
         } else {
             empHrs = 0;
         }
         empWage = empRatePerHr * empHrs;
-        System.out.println("Employee Wage=" + empWage);
+        System.out.println("Employee Wage =" + empWage);
     }
 }
