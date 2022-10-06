ea7340429d224562e2a61a8806aa67c4daeba593
VivekGujale <vdgujale007@.com>
2022-10-06 19:53:03 +0530

    added logic to calculate total Employee wage using class method

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,27 +1,22 @@
 public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
+    public static int EmpWage() {
         int isPartTime = 1;
         int isFullTime = 2;
         int empRatePerHr = 20;
-        int numWorkingDays = 20;
-        int empHrs, totalEmpHrs = 0, empWage, totalEmpWage;
-        int MaxHrsInMonth = 100;
-        int totalWorkingDays = 0;
-        while (totalEmpHrs <= MaxHrsInMonth && totalWorkingDays <= numWorkingDays) {
-            totalWorkingDays++;
-            int empCheck = (int) Math.floor(Math.random() * 10 % 3);
-            if (empCheck == isPartTime) {
-                empHrs = 8;
-            } else if (empCheck == isFullTime) {
-                empHrs = 16;
-            } else {
-                empHrs = 0;
-            }
-            totalEmpHrs = totalEmpHrs + empHrs;
-            System.out.println("Days:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+        int empHrs;
+        double empCheck = Math.floor(Math.random() * 10 % 3);
+        if (empCheck == isPartTime) {
+            empHrs = 8;
+        } else if (empCheck == isFullTime) {
+            empHrs = 16;
+        } else {
+            empHrs = 0;
         }
-        totalEmpWage = totalEmpHrs * empRatePerHr;
-        System.out.println("Total Employee Wage for month = " + totalEmpWage);
+        return empRatePerHr * empHrs;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage");
+        System.out.println("Employee Wage =" + EmployeeWage.EmpWage());
     }
 }
