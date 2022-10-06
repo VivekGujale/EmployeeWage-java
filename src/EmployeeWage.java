39e6bdd65ae380f583e7b415d121e92057cd0150
VivekGujale <vdgujale007@.com>
2022-10-06 19:11:51 +0530

    added logic to calculate Employee wage assuming fulltime 8

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,12 +1,16 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        int isEmpPresent = 1;
+        int isFullTime = 1;
+        int empRatePerHr = 20;
+        int empHrs, empWage = 0;
         double empCheck = Math.floor(Math.random() * 10 % 2);
-        if (isEmpPresent == empCheck) {
-            System.out.println("Employee is Present");
+        if (empCheck == isFullTime) {
+            empHrs = 8;
         } else {
-            System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        empWage = empRatePerHr * empHrs;
+        System.out.println("Employee Wage=" + empWage);
     }
 }
