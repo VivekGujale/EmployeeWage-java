59f8cdced8d7abb9c03bdf7434b1a12dc95e137d
VivekGujale <vdgujale007@.com>
2022-10-06 19:30:34 +0530

    added logic to check Employee wage for a month

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,22 +1,23 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
-        final int isPartTime = 1;
-        final int isFullTime = 2;
+        int isPartTime = 1;
+        int isFullTime = 2;
         int empRatePerHr = 20;
-        int empHrs, empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10 % 3);
-        switch (empCheck) {
-            case isPartTime:
+        int numWorkingDays = 20;
+        int empHrs, empWage = 0, totalEmpWage = 0;
+        double empCheck = Math.floor(Math.random() * 10 % 3);
+        for (int day = 1; day <= 20; day++) {
+            if (empCheck == isPartTime) {
                 empHrs = 8;
-                break;
-            case isFullTime:
+            } else if (empCheck == isFullTime) {
                 empHrs = 16;
-                break;
-            default:
+            } else {
                 empHrs = 0;
+            }
+            empWage = empRatePerHr * empHrs;
+            totalEmpWage = totalEmpWage + empWage;
         }
-        empWage = empRatePerHr * empHrs;
-        System.out.println("Employee Wage=" + empWage);
+        System.out.println("Total Employee Wage for month =" + totalEmpWage);
     }
 }
